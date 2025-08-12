e174b2a54eba52331f09bb32a64ce33dd3b02202
Jhamyllie <milli.santana2017@gmail.com>
2025-08-12 10:18:19 -0300

    fix: ajusta função para testar

--- src/implementacao/4-contestScore.ts
+++ src/implementacao/4-contestScore.ts
@@ -1,7 +1,15 @@
 import data from "../db/4-candidateData.json";
 
-export function listApproved():string[] {
-    return data.participants
+export function listApproved(candidates = data.participants):string[] {
+    return candidates
     .filter(candidate => candidate.score > 70)
     .map(candidate => `${candidate.name} obteve mais de 70 pontos.`);
 }
+
+console.log(listApproved( [
+    { "name": "João", "score": 75 },
+    // { "name": "Maria", "score": 80 },
+    // { "name": "Carlos", "score": 37 },
+    // { "name": "Steve", "score": 75 },
+    // { "name": "Marcela", "score": 92 }
+  ]));
